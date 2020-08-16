f37e916c00cc34cc39e0e281a6841b217b0e06ac
Ailan Quintero <ailanquinteroh@gmail.com>
2020-08-16 15:34:42 -0300

    creado formulario de login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,32 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
 
 });
+
+var correo = document.getElementById("inputEmail");
+var contra = document.getElementById("inputPassword");
+var errorCorreo = document.getElementById("demo");
+var errorContraseña = document.getElementById("demo1");
+
+function validarForm() {
+
+    var mensajeError = [];
+
+    errorCorreo.innerHTML = "";
+    errorContraseña.innerHTML = "";
+
+    mensajeError["Correo"] = correo.value === null || correo.value === "";
+    mensajeError["Contraseña"] = contra.value === null || contra.value === "";
+
+    if (mensajeError["Correo"]) {
+        errorCorreo.innerHTML = "Ingresa el correo";
+    }
+
+    if (mensajeError["Contraseña"]) {
+        errorContraseña.innerHTML = "Ingresa la contraseña";
+    }
+
+    return !mensajeError["Correo"] && !mensajeError["Contraseña"];
+};
